5c4613d76a8bf2237123f45b1e2fdd097437668e
Claus Thrane <cth@tradeshift.com>
2015-07-01 21:38:49 +0800

    Adds CORS headers to OPTION requests

--- api/handlers.go
+++ api/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	traindata "github.com/clausthrane/futfut/traindata"
 	"github.com/gorilla/mux"
+	//	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,20 +12,41 @@ import (
 
 var logger = log.New(os.Stdout, " ", log.Ldate|log.Ltime|log.Lshortfile)
 
+type handlerWrapper func(http.Handler) http.HandlerFunc
+
 func NewHandler() http.Handler {
-	router := mux.NewRouter()
-	router.HandleFunc("/api/stations", stationsHandler())
-	return router
+	api := mux.NewRouter()
+	api.HandleFunc("/api/stations", chainHandlers(stationsHandler, allowCORS))
+	api.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/")))
+	return api
+}
+
+func chainHandlers(handler http.HandlerFunc, others ...handlerWrapper) http.HandlerFunc {
+	for _, other := range others {
+		handler = other(handler)
+	}
+	return handler
+}
+
+func stationsHandler(w http.ResponseWriter, r *http.Request) {
+	allStations, err := traindata.GetStations()
+	if err == nil {
+		logger.Println("Listing stations", len(allStations.Stations))
+		encoder := json.NewEncoder(w)
+		encoder.Encode(allStations.Stations)
+	}
 
 }
 
-func stationsHandler() func(http.ResponseWriter, *http.Request) {
+func allowCORS(handler http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		allStations, err := traindata.GetStations()
-		if err == nil {
-			logger.Println("Listing stations", len(allStations.Stations))
-			encoder := json.NewEncoder(w)
-			encoder.Encode(allStations.Stations)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+			w.Header().Set("Access-Control-Max-Age", "1000")
+			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		} else {
+			handler.ServeHTTP(w, r)
 		}
 	}
 }
