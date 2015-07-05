6ff0532b7d5ccb0e36fe612e8863035fc233e0c8
Claus Thrane <cth@tradeshift.com>
2015-07-05 18:32:38 +0800

    Adds Stringer interface to train model, and several data methods

--- models/train.go
+++ models/train.go
@@ -1,5 +1,25 @@
 package models
 
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+var logger = log.New(os.Stdout, " ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+var numberSequenceRegex *regexp.Regexp
+
+func init() {
+	numberSequenceRegex, _ = regexp.Compile("([0-9]+)")
+}
+
+// Train respresents a train event
+//
+// E.g. when does a certain train (by TrainNumber) arrive at station Station Uic
 type Train struct {
 	ID                     string `UUID`
 	StationUic             string `int`
@@ -15,11 +35,52 @@ type Train struct {
 	ArrivalDelay           string `int`
 	DepartureDelay         string `int`
 	Cancelled              bool
-	Line                   string `obj?`
-	Direction              string `North, south, ..`
+	Line                   string
+	Direction              string `Nord : North, Syd: South, ..`
 	MinutesToDeparture     string `int`
+}
+
+func (t *Train) String() string {
+	return fmt.Sprintf("Train : %s expected at Station %s at %s leaving at %s",
+		t.TrainNumber,
+		t.StationUic,
+		t.HumanReadableArrivalDate(),
+		t.HumanReadableDepartureDate())
+}
+
+func (t *Train) arrivesBefore(other *Train) bool {
+	return humanReadableDate(t.ScheduledArrival).Before(humanReadableDate(other.ScheduledArrival))
+}
+
+func (t *Train) HumanReadableArrivalDate() string {
+	return humanReadableDateString(t.ScheduledArrival)
+}
+
+func (t *Train) HumanReadableDepartureDate() string {
+	return humanReadableDateString(t.ScheduledDeparture)
+}
+
+func humanReadableDateString(date string) string {
+	return humanReadableDate(date).String()
+}
+
+func humanReadableDate(date string) time.Time {
+	if strNum := numberSequenceRegex.FindString(date); len(strNum) > 0 {
+		if milis, err := strconv.Atoi(strNum); err == nil {
+			return time.Unix(int64(milis/1000), 0)
+		}
+	}
+	return time.Time{}
 }
 
 type TrainList struct {
 	Trains []Train
+}
+
+func (tl *TrainList) String() string {
+	var buffer bytes.Buffer
+	for _, t := range tl.Trains {
+		buffer.WriteString(fmt.Sprintf("[ %s ]", t.String()))
+	}
+	return buffer.String()
 }
---
+++ models/train_test.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"github.com/clausthrane/futfut/utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHumanReadableDateConversion(t *testing.T) {
+	assert := assert.New(t)
+	in := "/Date(1436068203504)/"
+	out := humanReadableDate(in)
+	assert.Equal("2015-07-05 03:50:03 +0000 UTC", out.UTC().String())
+}
+
+func TestHumanReadableArrivalDate(t *testing.T) {
+	assert := assert.New(t)
+
+	train := Train{"event_id1",
+		"station_id2",
+		"type",
+		"end_dest",
+		123,
+		"track1",
+		"",
+		"Train2",
+		"86",
+		"/Date(1436068203504)/",
+		"",
+		"",
+		"",
+		false,
+		"",
+		"",
+		"",
+	}
+
+	out := train.HumanReadableArrivalDate()
+	assert.Equal("2015-07-05 03:50:03 +0000 UTC", utils.ParseDateString(out).UTC().String())
+}
