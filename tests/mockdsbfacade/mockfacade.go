e3a980979e0ce2f43bfde749968aed1c12a30ed1
Claus Thrane <cth@tradeshift.com>
2015-07-07 13:05:19 +0800

    Updates the mock DSB facade to implement the corresponding interface

--- tests/mockdsbfacade/mockfacade.go
+++ tests/mockdsbfacade/mockfacade.go
@@ -14,6 +14,16 @@ func (m MockDSB) GetStations() (chan *models.StationList, chan error) {
 	return args.Get(0).(chan *models.StationList), args.Get(1).(chan error)
 }
 
+func (m MockDSB) GetStation(stationId string) (chan *models.StationList, chan error) {
+	args := m.Called(stationId)
+	return args.Get(0).(chan *models.StationList), args.Get(1).(chan error)
+}
+
+func (m MockDSB) GetAllTrains() (chan *models.TrainEventList, chan error) {
+	args := m.Called()
+	return args.Get(0).(chan *models.TrainEventList), args.Get(1).(chan error)
+}
+
 func (m MockDSB) GetTrains(key string, value string) (chan *models.TrainEventList, chan error) {
 	args := m.Called(key, value)
 	return args.Get(0).(chan *models.TrainEventList), args.Get(1).(chan error)
