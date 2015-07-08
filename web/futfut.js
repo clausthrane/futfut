9cd2c5990f14d59ecbf17490bfb67be1d3e45f91
Claus Thrane <cth@tradeshift.com>
2015-07-08 13:20:44 +0800

    Adds visual indication of errors

--- web/futfut.js
+++ web/futfut.js
@@ -1,19 +1,27 @@
 function FutfutCtrl($scope, $http) {
 
+    console.log("ddd")
+
     $http.get('/api/v1/stations').
         success(function (data) {
             $scope.stationCount = data.Count
             $scope.stations = data.Stations;
             $scope.traindata = undefined
+        }).error(function (error, status) {
+            handleError(error, status)
         });
 
 
     $scope.chooseStation = function (stationId) {
+        $scope.error = undefined
         $http.get('/api/v1/departures/from/' + stationId).
             success(function (departures) {
                 $scope.departures = departures
                 $scope.traindata = undefined
-            })
+            }).error(function (error) {
+                console.log("sss")
+                $scope.error = error
+            });
     }
 
     $scope.chooseDeparture = function (trainId) {
@@ -21,6 +29,17 @@ function FutfutCtrl($scope, $http) {
             success(function (trains) {
                 $scope.trainId = trainId
                 $scope.traindata = trains
-            })
+            }).error(function (error) {
+                console.log("sss")
+                $scope.error = error
+            });
+    }
+
+    function handleError(error, status) {
+        if (status >= 500) {
+            $scope.systemerror = error
+        } else {
+            $scope.temperror = error
+        }
     }
 }
