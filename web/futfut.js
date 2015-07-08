baebf75b237ba29623e063491c88b0ddb962195f
Claus Thrane <cth@tradeshift.com>
2015-07-08 12:39:02 +0800

    Revamps UI
    Adds a bit of styling and integrates new DTOs

--- web/futfut.js
+++ web/futfut.js
@@ -1,14 +1,15 @@
 function FutfutCtrl($scope, $http) {
 
-    $http.get('/api/stations').
+    $http.get('/api/v1/stations').
         success(function (data) {
             $scope.stationCount = data.Count
             $scope.stations = data.Stations;
+            $scope.traindata = undefined
         });
 
 
     $scope.chooseStation = function (stationId) {
-        $http.get('/api/departures/from/' + stationId).
+        $http.get('/api/v1/departures/from/' + stationId).
             success(function (departures) {
                 $scope.departures = departures
                 $scope.traindata = undefined
@@ -16,7 +17,7 @@ function FutfutCtrl($scope, $http) {
     }
 
     $scope.chooseDeparture = function (trainId) {
-        $http.get('/api/trains/' + trainId).
+        $http.get('/api/v1/trains/' + trainId).
             success(function (trains) {
                 $scope.trainId = trainId
                 $scope.traindata = trains
