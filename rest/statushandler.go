1c94f8a1209eb846043755dee0a5e319403ee493
Claus Thrane <cth@tradeshift.com>
2015-07-09 22:22:58 +0200

    Adds general health flag to the status resource

--- rest/statushandler.go
+++ rest/statushandler.go
@@ -9,10 +9,11 @@ import (
 
 func (h *RequestHandler) HandleStatusRequest(w http.ResponseWriter, r *http.Request) error {
 	counters, guages := metrics.Snapshot()
-	return writeJsonResponse(w, Status{counters, guages, viper.AllSettings()}, nil)
+	return writeJsonResponse(w, Status{true, counters, guages, viper.AllSettings()}, nil)
 }
 
 type Status struct {
+	IsUP     bool
 	Counters map[string]uint64
 	Guages   map[string]int64
 	Config   map[string]interface{}
